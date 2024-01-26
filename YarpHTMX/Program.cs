573fb09d904a90bd20b60e62b92346d2602ba2df
hogberg <andre.hogberg@winlas.se>
2024-01-26 13:22:59 +0100

    remove auth stuff

--- YarpHTMX/Program.cs
+++ YarpHTMX/Program.cs
@@ -1,44 +1,10 @@
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.IdentityModel.Tokens;
-using Zitadel.Authentication;
-using Zitadel.Extensions;
-
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Configuration.AddJsonFile("secrets.json", false);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddAuthorization();
-builder.Services.AddAuthentication(ZitadelDefaults.AuthenticationScheme)
-    .AddZitadel(opt =>
-    {
-        opt.SignInScheme = IdentityConstants.ExternalScheme;
-        
-        opt.Authority = builder.Configuration["Zitadel:Authority"];
-        opt.ClientId = builder.Configuration["Zitadel:ClientId"];
-        
-        opt.GetClaimsFromUserInfoEndpoint = true;
-        
-        opt.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidIssuer = opt.Authority,
-            ValidAudience = opt.ClientId
-        };
-    })
-    .AddExternalCookie()
-    .Configure(opt =>
-    {
-        opt.AccessDeniedPath = "/login";
-        opt.LoginPath = "/login";
-        opt.Cookie.HttpOnly = true;
-        opt.Cookie.SameSite = SameSiteMode.None;
-        opt.Cookie.SecurePolicy = CookieSecurePolicy.Always;
-    });
 
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
@@ -52,16 +18,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseAuthentication();
-app.UseAuthorization();
 
 app.MapReverseProxy();
 
 app.UseHttpsRedirection();
-
-app.MapGet("/login", async context =>
-{
-    await context.ChallengeAsync(ZitadelDefaults.AuthenticationScheme, new() { RedirectUri = "/app2" });
-});
-
 app.Run();
